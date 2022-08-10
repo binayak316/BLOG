7a8e36822c0e8e277f58a49fa8aa8563c4eb5f97
binayak316 <binayakpokhrel005@gmail.com>
2022-08-10 22:52:57 +0545

    some ui change

--- djangoBLOGproject/settings.py
+++ djangoBLOGproject/settings.py
@@ -129,9 +129,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/' #url dekhine hoo
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'blog/media/')
