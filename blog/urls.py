64370967b8d02cbc6dbcde7f1abb56b8131bf7e0
binayak316 <binayakpokhrel005@gmail.com>
2022-10-07 17:24:33 +0545

    upto make a new password incase user forget password

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -12,5 +13,14 @@ urlpatterns = [
 
     path('post_edit/<int:pk>', views.post_edit, name="blog-post-edit"),
     path('post_delete/<int:pk>', views.post_delete, name="blog-post-delete"),
+
+    #class based views for forget password
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='blog/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_view.PasswordResetDoneView.as_view(template_name='blog/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name='blog/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name='blog/password_reset_complete.html'), name='password_reset_complete'),
+
+
+
     
 ]
--- djangoBLOGproject/settings.py
+++ djangoBLOGproject/settings.py
@@ -143,3 +143,11 @@ LOGIN_URL = 'login-user'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'binayakpokhrel005@gmail.com'
+EMAIL_HOST_PASSWORD = 'tswtrgbvclgaunib'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
